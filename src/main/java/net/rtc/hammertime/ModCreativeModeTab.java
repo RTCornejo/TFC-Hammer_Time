443c93df51f3f929539e730810295ba611805821
RTCornejo <138426772+RTCornejo@users.noreply.github.com>
2024-09-24 18:18:48 -0300

    Added excavators, heads, and molds to Creative Tabs

--- src/main/java/net/rtc/hammertime/ModCreativeModeTab.java
+++ src/main/java/net/rtc/hammertime/ModCreativeModeTab.java
@@ -9,14 +9,26 @@ import net.minecraftforge.registries.RegistryObject;
 import net.rtc.hammertime.common.items.ModItems;
 
 public class ModCreativeModeTab {
-    public static void AddCreative(BuildCreativeModeTabContentsEvent event){
-        if (event.getTabKey() == TFCCreativeTabs.METAL.tab().getKey()){
-            for (Metal.Default metal : Metal.Default.values()){
-                RegistryObject<Item> item = ModItems.SLEDGEHAMMERS.get(metal);
-                if (item != null && item.isPresent()){
-                    event.accept(item);
+    public static void AddCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == TFCCreativeTabs.METAL.tab().getKey()) {
+            for (Metal.Default metal : Metal.Default.values()) {
+                RegistryObject<Item> sledgehammer = ModItems.SLEDGEHAMMERS.get(metal);
+                RegistryObject<Item> excavator = ModItems.EXCAVATORS.get(metal);
+                RegistryObject<Item> sledgehammer_heads = ModItems.SLEDGEHAMMER_HEADS.get(metal);
+                RegistryObject<Item> excavator_heads = ModItems.EXCAVATOR_HEADS.get(metal);
+                if (sledgehammer != null && sledgehammer.isPresent()) {
+                    event.accept(sledgehammer);
+                    event.accept(sledgehammer_heads);
+                    event.accept(excavator);
+                    event.accept(excavator_heads);
                 }
             }
+        }
+        if (event.getTabKey() == TFCCreativeTabs.MISC.tab().getKey()){
+            event.accept(ModItems.UNFIRED_EXCAVATOR_HEAD_MOLD.get());
+            event.accept(ModItems.EXCAVATOR_HEAD_MOLD.get());
+            event.accept(ModItems.UNFIRED_SLEDGEHAMMER_HEAD_MOLD.get());
+            event.accept(ModItems.SLEDGEHAMMER_HEAD_MOLD.get());
         }
     }
 }
