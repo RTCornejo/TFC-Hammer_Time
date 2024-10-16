b6ed896442c40011f430f0ad3742eacf3ddef883
RTCornejo <138426772+RTCornejo@users.noreply.github.com>
2024-10-16 18:36:17 -0300

    Update SledgeItem to take layer into consideration
    No more lost charcoal (or snow)

--- src/main/java/net/rtc/hammertime/common/items/SledgeItem.java
+++ src/main/java/net/rtc/hammertime/common/items/SledgeItem.java
@@ -4,7 +4,6 @@ import net.dries007.tfc.common.items.CreativeMiningTool;
 import net.dries007.tfc.common.items.ToolItem;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.LivingEntity;
@@ -15,14 +14,22 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.level.BlockEvent;
 
 public class SledgeItem extends ToolItem implements CreativeMiningTool {
 
+    private final TagKey<Block> blocks;
+
     public SledgeItem(Tier tier, float attackDamage, float attackSpeed, TagKey<Block> mineableBlocks, Item.Properties properties) {
         super(tier, attackDamage, attackSpeed, mineableBlocks, properties);
+        this.blocks = mineableBlocks;
+    }
 
+    public float getDestroySpeed(ItemStack pStack, BlockState pState) {
+        return pState.is(this.blocks) ? (this.speed * 0.4f) : 1.0F;
     }
 
     public void mineBlockInCreative(ItemStack stack, Level level, BlockState state, BlockPos pos, Player player) {
@@ -32,13 +39,19 @@ public class SledgeItem extends ToolItem implements CreativeMiningTool {
     public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos origin, LivingEntity entity) {
         this.doSledgeMining(stack, level, state, origin, entity);
         return super.mineBlock(stack, level, state, origin, entity);
+    }
 
+    private void dropResourcesAndBreak(BlockState state, BlockPos pos, Player player, ItemStack stack, Level level) {
+        if (!player.isCreative()) {
+            Block.dropResources(state, level, pos, state.hasBlockEntity() ? level.getBlockEntity(pos) : null, player, player.getMainHandItem());
+            stack.hurtAndBreak(1, player, (p) -> {
+                p.broadcastBreakEvent(player.getUsedItemHand());
+            });
+        }
     }
 
     private void doSledgeMining(ItemStack stack, Level level, BlockState state, BlockPos origin, LivingEntity entity) {
         if (entity instanceof ServerPlayer player) {
-
-            entity.sendSystemMessage(Component.literal(String.valueOf(getDestroySpeed(stack, state))));
 
             BlockHitResult hitResult = (BlockHitResult) player.pick(20.0D, 0.0F, false);
             Direction face = hitResult.getDirection();  // Get the face of the block that was hit
@@ -67,23 +80,28 @@ public class SledgeItem extends ToolItem implements CreativeMiningTool {
             };
             // Adjust the 3x3x1 area based on the face the player is mining
 
+            BlockState originBlock = level.getBlockState(origin);
             for (BlockPos pos : BlockPos.betweenClosed(startPos, endPos)) {
                 BlockState stateAt = level.getBlockState(pos);
-                BlockState originBlock = level.getBlockState(origin);
                 if (!pos.equals(origin)) {
-                    BlockEvent.BreakEvent event = new BlockEvent.BreakEvent(level, pos, stateAt, player);
-                    if (!MinecraftForge.EVENT_BUS.post(event)) {
-                        if (this.isCorrectToolForDrops(stack, stateAt) && this.isCorrectToolForDrops(stack, originBlock)) {
-                            if (!player.isCreative()) {
-                                Block.dropResources(stateAt, level, pos, stateAt.hasBlockEntity() ? level.getBlockEntity(pos) : null, player, player.getMainHandItem());
-                                stack.hurtAndBreak(1, player, (p) -> {
-                                    p.broadcastBreakEvent(player.getUsedItemHand());
-                                });
+                    if (this.isCorrectToolForDrops(stack, stateAt) && this.isCorrectToolForDrops(stack, originBlock)) {
+                        BlockEvent.BreakEvent event = new BlockEvent.BreakEvent(level, pos, stateAt, player);
+                        if (!MinecraftForge.EVENT_BUS.post(event)) {
+                            if (stateAt.hasProperty(BlockStateProperties.LAYERS)) {
+                                int layers = stateAt.getValue(BlockStateProperties.LAYERS);
+                                if (layers > 1) {
+                                    level.setBlock(pos, stateAt.setValue(BlockStateProperties.LAYERS, layers - 1), 3);
+                                } else {
+                                    level.destroyBlock(pos, false, player);
+                                }
+                                dropResourcesAndBreak(stateAt, pos, player, stack, level);
+                            } else {
+                                dropResourcesAndBreak(stateAt, pos, player, stack, level);
+                                level.destroyBlock(pos, false, player);
                             }
-                            level.destroyBlock(pos, false, player);
-                            if (stack.getDamageValue() >= stack.getMaxDamage()) {
-                                break;
-                            }
+                        }
+                        if (stack.getDamageValue() >= stack.getMaxDamage()) {
+                            break;
                         }
                     }
                 }
